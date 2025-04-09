d208b12d8ff9ceb429e679e4f6e646396d0fd0d6
redbeanbread031 <readbeanbread031@gmail.com>
2025-04-09 20:24:13 +0900

    feat: set-directory

---
+++ consumer/src/main/java/kr/cs/interdata/consumer/ConsumerApplication.java
@@ -0,0 +1,13 @@
+package kr.cs.interdata.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+}
---
+++ producer/src/test/java/kr/cs/interdata/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.cs.interdata.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
