07c1b9650302c18723e2d22d6735ce0c49aa09a5
redbeanbread031 <readbeanbread031@gmail.com>
2025-04-25 05:26:43 +0900

    feat: threshold 조회 및 초과 시 API 로그 전송 기능 추가

--- consumer/src/main/java/kr/cs/interdata/consumer/ConsumerApplication.java
+++ consumer/src/main/java/kr/cs/interdata/consumer/ConsumerApplication.java
@@ -2,12 +2,11 @@ package kr.cs.interdata.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-
+@EnableScheduling
 @SpringBootApplication
 public class ConsumerApplication {
-
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerApplication.class, args);
