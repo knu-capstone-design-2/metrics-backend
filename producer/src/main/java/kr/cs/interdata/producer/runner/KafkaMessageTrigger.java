94da70fb9284c5be18fe07f959aaee15a73377de
차예원 <chayewon@chayewon-ui-noteubug.local>
2025-05-06 22:34:17 +0900

    modify root build.gradle

--- producer/src/main/java/kr/cs/interdata/producer/runner/KafkaMessageTrigger.java
+++ producer/src/main/java/kr/cs/interdata/producer/runner/KafkaMessageTrigger.java
@@ -1,16 +1,20 @@
 package kr.cs.interdata.producer.runner;
 
+import com.google.gson.Gson;
 import kr.cs.interdata.producer.service.KafkaProducerService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.producer.KafkaProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+
 @Component
+@Profile("!test")
 @Slf4j
-public class KafkaMessageTrigger {
+public class KafkaMessageTrigger implements CommandLineRunner {
 
     private final KafkaProducerService kafkaProducerService;
 
@@ -19,13 +23,50 @@ public class KafkaMessageTrigger {
         this.kafkaProducerService = kafkaProducerService;
     }
 
-    /***
-     *
-     * @param jsonPayload
-     *
-     * data-collector가 JSON을 보낼 때마다 호출
-     */
-    public void processJsonMessage(String jsonPayload) {
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Run KafkaMessageTrigger");
+
+        String jsonPayload = """
+                {
+                    "type": "host",
+                    "hostId": "test-host-id",
+                    "cpuUsagePercent": 75.0,
+                     "memoryTotalBytes": 16000000000,
+                     "memoryUsedBytes": 12000000000
+                }
+        """;
+
         kafkaProducerService.routeMessageBasedOnType(jsonPayload);
+
+//        startRealTimeProcessing();
     }
+
+//    /***
+//     * 실시간 데이터 처리 : 1초마다 전송
+//     */
+//    public void startRealTimeProcessing() {
+//        new Thread(() -> {
+//            while (true) {
+//                String jsonPayload = getJsonFromDataCollector();
+//
+//                kafkaProducerService.routeMessageBasedOnType(jsonPayload);
+//
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
+//    }
+//
+//    /***
+//     * 데이터를 받아오는 로직 필요
+//     * @return
+//     */
+//    private String getJsonFromDataCollector() {
+//        // Mock Data
+//        return "{ \"type\": \"host\", \"cpu\": 45, \"memory\": 1024 }";
+//    }
 }
